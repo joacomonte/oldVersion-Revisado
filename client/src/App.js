87d1c637614fa1416e25453ffb382a76214ec6b3
Joaquin Montesano <montesano1994@gmail.com>
2022-09-27 17:40:04 -0300

    updatee carousel4

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import RevisadoTitle from '.././components/RevisadoTitle/RevisadoTitle';
-import Shop from '.././components/Shop/Shop';
-import Confiar from '.././components/Confiar/Confiar';
-import Navbar from '../components/Navbar/Navbar';
+import RevisadoTitle from './components/RevisadoTitle/RevisadoTitle';
+import Shop from './components/Shop/Shop';
+import Confiar from './components/Confiar/Confiar';
+import Navbar from './components/Navbar/Navbar';
 import {Routes, Route} from 'react-router-dom'
-import Sliders from '../components/SliderProducts/SliderProducts';
+import Sliders from './components/SliderProducts/SliderProducts';
 
 function App() {
   return (
